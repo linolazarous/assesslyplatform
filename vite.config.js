7402178ba0520733c04766c54eefa7b293289e3b
linolazarous <linolaku560@gmail.com>
2025-09-24 13:05:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // You can add more configurations here, like proxy rules for API calls
+
   server: {
     proxy: {
-      '/api': 'http://localhost:3000' // Example proxy for your Vercel functions
+      '/api': 'http://localhost:5000' 
     }
   }
 });
