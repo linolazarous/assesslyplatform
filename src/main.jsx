0bfd630ffe1d7fd45d65050ce1f5b24dbc1e5633
linolazarous <linolaku560@gmail.com>
2025-10-08 13:59:54 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
-import theme from "./theme";
-import { AuthProvider } from "./contexts/AuthContext";
-import ErrorBoundary from "./ErrorBoundary";
-
 import "./styles/global.css";
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <AuthProvider>
-          <ErrorBoundary>
-            <App />
-          </ErrorBoundary>
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
