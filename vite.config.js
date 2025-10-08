6a1e8223eb6217be13849186aa95bd9d9bab3f3e
linolazarous <linolaku560@gmail.com>
2025-10-08 14:04:31 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  root: ".", // ✅ because index.html is in root
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  preview: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
