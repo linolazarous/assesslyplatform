9d68a6456a94cd3d9961d9b24f098946d1f3a163
linolazarous <linolaku560@gmail.com>
2025-10-09 14:44:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./styles/global.css";
-import ErrorBoundary from "./ErrorBoundary";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
 // Enable React 18 root API with error boundaries for safety
 const rootElement = document.getElementById("root");
