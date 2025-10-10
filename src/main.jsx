ac60b0edbf582ec1c4a6fccfe0cc436ad9c41061
linolazarous <linolaku560@gmail.com>
2025-10-10 23:49:43 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
+import "./styles/theme.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 
 const rootElement = document.getElementById("root");
