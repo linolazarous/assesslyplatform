2406461057641367ec003ef2ff5e9a3c929516ef
linolazarous <linolaku560@gmail.com>
2025-11-01 18:57:51 +0200

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -29,8 +29,8 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, 'Password is required'],
-      minlength: [8, 'Password must be at least 8 characters'],
-      select: false
+      minlength: [8, 'Password must be at least 8 characters']
+      // REMOVED: select: false - This was breaking authentication
     },
 
     role: {
